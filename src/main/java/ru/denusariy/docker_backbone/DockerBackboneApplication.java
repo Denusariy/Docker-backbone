04189925c2605778cf5d475181f67ce0ef1cb748
Denis Anikeenko <d.anikeenko@abflow.ru>
2024-09-18 17:23:55 +0500

    Initial commit

---
+++ src/main/java/ru/denusariy/docker_backbone/DockerBackboneApplication.java
@@ -0,0 +1,13 @@
+package ru.denusariy.docker_backbone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerBackboneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerBackboneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/denusariy/docker_backbone/DockerBackboneApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.denusariy.docker_backbone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerBackboneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
